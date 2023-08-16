500e1d2f0af94e953c4994d65cdc0ef9961ec039
yueqiao <yueqiaozgb@163.com>
2023-08-16 18:45:03 +0800

    添加axios

--- YueBlog-cms/vite.config.ts
+++ YueBlog-cms/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import * as path from "path";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, "./src"),
+    }
+  },
+  server:{
+    proxy:{
+      '/api':{
+        target: 'http://127.0.0.1:8080/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
