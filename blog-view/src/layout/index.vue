50abf5f283dbf6319bc92a4baba504301ebfeebd
yueqiao <yueqiaozgb@163.com>
2025-04-03 11:22:20 +0800

    1.blog-view:引入fomantic-ui等依赖

--- blog-view/src/layout/index.vue
+++ blog-view/src/layout/index.vue
@@ -1,6 +1,11 @@
 <script setup>
+
 </script>
 
 <template>
-  <router-view></router-view>
+
 </template>
+
+<style scoped>
+
+</style>
--- blog-view/src/main.js
+++ blog-view/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+// 引入路由
+import router from "./router";
+// 引入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 引入fomantic-ui
+import 'fomantic-ui/dist/semantic.min.css'
+import 'fomantic-ui/dist/semantic.min.js'
+// 引入jquery, fomantic-ui依赖
+import $ from 'jquery'
 
-createApp(App).mount('#app')
+window.$ = $
+
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
